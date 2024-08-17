d911931ff2e6e549ec0653120951ebe48feb4326
Olga Blud <olga.blud@gmail.com>
2024-08-17 10:45:50 +0200

    render countries

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,33 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { getCountries } from 'service/countryApi';
 
 const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const data = await getCountries();
+        setCountries(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  // console.log(countries);
   return (
     <Section>
       <Container>
-        <Heading title="Home" bottom />
+        {countries.length > 0 && <CountryList countries={countries} />}
+        {isLoading && <Loader />}
+        {error && <Heading title="Something went wrong ..." bottom />}
       </Container>
     </Section>
   );
